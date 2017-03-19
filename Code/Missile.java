3c17c714fd664b16d8dd7783da2959e3645f71a7
quelquune <chloe.cros618@gmail.com>
2017-03-19 18:21:24 +0100

    Update Missile.java

--- Code/Missile.java
+++ Code/Missile.java
@@ -1,6 +1,10 @@
 import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 import javax.swing.JFrame;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
 
 public class Missile extends ObjetMobile {
 	
@@ -9,28 +13,35 @@ public class Missile extends ObjetMobile {
     
     
     public Missile(int x) {    // x personnage
-        super(x, 0);
-        taille = 0;
+        super(x,0);
+        taille = 200;
         largeur = 10;
+        try {
+             imagePointe= ImageIO.read(new File("pointe1.png"));
+        } catch(Exception err) {
+            System.out.println("pointe.jpg" +" introuvable !");            
+            System.exit(0);
+        } 
         hauteur = taille + imagePointe.getHeight();
     }
+    
 	
     public int getTaille(){
 		return taille;
 	}
 	
-    public void dessiner(Graphics g, JFrame jf){
+    public void dessiner(Graphics g , JFrame jf){
         y = jf.getHeight() - taille - imagePointe.getHeight();
-        g.drawImage(imagePointe, (int)x, (int)y, jf);
+        g.drawImage(imagePointe, x, y, jf);
         g.fillRect((int)(x + imagePointe.getWidth()/2 - largeur/2), jf.getHeight() - taille , largeur, taille);
     }
 
-    public boolean bouger(){
+    public void bouger(){
         if(y>0){
             taille += vitesse;
-            return true;
+            //return true;
         }
-        return false;
+        //return false;
     }
 	
 }
