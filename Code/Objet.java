e983f4bd6272df7a97b0cf392925a307c45b9af3
quelquune <chloe.cros618@gmail.com>
2017-03-20 15:42:24 +0100

    Update Objet.java

--- Code/Objet.java
+++ Code/Objet.java
@@ -14,48 +14,44 @@ public class Objet {
 	
 	protected int x; 
     protected int y;
-    protected int hauteur;
-    protected int largeur;
+    protected int hauteur;  // verif
+    protected int largeur;  // vérifier utilisée
     protected int vitesse;
-     /** La direction (angle par rapport à l'axe horizontal et le coin haut gauche)*/
-    protected int direction;
+     /** La direction (angle par rapport à l'axe horizontal*/
+    protected double direction;
     protected BufferedImage image;
     
     
-    // choisir les construteurs
-    public Objet(int x, int y, int hauteur, int largeur){
+    public Objet(int x, int y, String nomImage, double direction, int vitesse){
         this.x = x;
         this.y = y;
-        this.hauteur = hauteur;
-        this.largeur = largeur;
+        try {
+             image= ImageIO.read(new File(nomImage));
+        } catch(Exception err) {
+            System.out.println(nomImage +" introuvable !");            
+            System.exit(0);
+        } 
+        this.direction = direction;
+        this.vitesse = vitesse;
+        this.largeur = image.getWidth();
+        this.hauteur = image.getHeight();
     }
     
-    public Objet(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-    
-    public Objet(String nbFichier, int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-        
-    public Objet(){
-    }   
     
     
     public int getX(){
         return x;
     }
-    
+    /*
     public int getY(){
         return y;
-    }
+    }*/
     
     /** Methode pour bouger l'objet. Delta s'exprime en ms **/
-    public void bouger() {
-		x +=(int) vitesse*Math.cos(direction);
-		y +=(int) vitesse*Math.sin(direction);
+    public boolean bouger() {
+		x +=  vitesse*Math.cos(direction);
+		y +=  vitesse*Math.sin(direction);
+		return true;
 	}
     
     public boolean collision(Objet o){
