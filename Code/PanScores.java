54e924c173bf833a99c1d09e55b031faa2e65a23
MasterMaevis <gluealafraise@hotmail.fr>
2017-03-20 09:12:51 +0100

    Add files via upload

---
+++ Code/PanScores.java
@@ -0,0 +1,22 @@
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+
+public class PanScores extends PanAffichage{
+    
+    // Images
+    private Image imageFond;
+    private BufferedImage fond;
+    
+    public PanScores(int LARGEUR, int HAUTEUR){
+        super(LARGEUR, HAUTEUR);
+    }
+    
+    public void paintComponent(Graphics g){
+        
+    }
+}
