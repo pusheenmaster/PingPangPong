877f092d14041c396e6a19eec950257a726b3310
BabyOtter <MilieDef@gmail.com>
2017-03-20 09:56:57 +0100

    Update Balle.java

--- Code/Balle.java
+++ Code/Balle.java
@@ -1,5 +1,12 @@
 import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 import javax.swing.*;
+import java.lang.Math; 
+import java.lang.Object;
 
 public class Balle extends ObjetMobile {
 	
@@ -9,19 +16,25 @@ public class Balle extends ObjetMobile {
     private int hauteurMax;
     
     /** Balle qui part de gauche a droite **/
-	public Balle(String file, int x, int y, int size){
-        super((file += "balle_" + size + ".png"), x, y);
+	public Balle(int x, int y, int size){
+        super(("balle_"+size+".png"), x, y);
         this.size = size;
-        x = 200;  
-        y = 200;  
+        direction = (int)(new java.util.Radom()*2*Math.PI);
+        this.x=x;
+        this.y=y;
+        vitesse=10;
+        try {
+             image= ImageIO.read(new File("balle_"+size+".png"));
+        } catch(Exception err) {
+            System.out.println("fichier introuvable !");            
+            System.exit(0);
+        }
     }
     
     /** Si la balle doit aller dans l'autre direction **/
-    public Balle(String file, int x, int y, int size, boolean autreDirection){
-        super((file += "balle_" + size + ".png"), x, y);
-        this.size = size;
-        x = -200;  
-        y = 200;  
+    public Balle(int x, int y, int size, boolean autreDirection){
+        super(("balle_" + size + ".png"), x, y);
+        this.size = size; 
     }
     
     public int getX(){
@@ -32,6 +45,12 @@ public class Balle extends ObjetMobile {
     }
     public int getSize(){
     	return size;
+    }
+    public void setX(int nX){
+    	x=nX;
+    }
+    public void setY(int nY){
+    	y=nY;
     }
     
     public void bouger(JPanel p){
@@ -53,6 +72,11 @@ public class Balle extends ObjetMobile {
 		}
 		super.bouger();
 	}
+	
+	public void dessiner(Graphics g, JPanel jp){  // JPanel changements pour test
+		g.drawImage(image,(int)x,(int)y,jp);
+	}
 }
+
 
 
