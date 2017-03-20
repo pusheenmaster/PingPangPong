d0db60f80088a28869e63b30d79ad30c240ef213
quelquune <chloe.cros618@gmail.com>
2017-03-20 09:56:18 +0100

    Update Missile.java

--- Code/Missile.java
+++ Code/Missile.java
@@ -6,23 +6,22 @@ import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
 
-public class Missile extends ObjetMobile {
+public class Missile extends Objet {
 	
-    private BufferedImage imagePointe;  
     private int taille;
-    
     
     public Missile(int x) {    // x personnage
         super(x,0);
         taille = 200;
         largeur = 10;
         try {
-             imagePointe= ImageIO.read(new File("pointe1.png"));
+             image= ImageIO.read(new File("pointe1.png"));
         } catch(Exception err) {
             System.out.println("pointe.jpg" +" introuvable !");            
             System.exit(0);
         } 
-        hauteur = taille + imagePointe.getHeight();
+        hauteur = taille + image.getHeight();
+        
     }
     
 	
@@ -31,12 +30,14 @@ public class Missile extends ObjetMobile {
 	}
 	
     public void dessiner(Graphics g , JPanel jp){
-        y = jp.getHeight() - taille - imagePointe.getHeight();
-        g.drawImage(imagePointe, x, y, jp);
-        g.fillRect((int)(x + imagePointe.getWidth()/2 - largeur/2), jp.getHeight() - taille , largeur, taille);
+        y = jp.getHeight() - taille - image.getHeight();
+        super.dessiner(g, jp);
+        //g.drawImage(image, x, y, jp);
+        g.fillRect((int)(x + image.getWidth()/2 - largeur/2), jp.getHeight() - taille , largeur, taille);
     }
 
     public void bouger(){
+		
         if(y>0){
             taille += vitesse;
             //return true;
