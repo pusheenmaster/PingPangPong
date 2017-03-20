5a3f6dc4e1785c20f93e47dcc4d103d8bff510a5
MasterMaevis <gluealafraise@hotmail.fr>
2017-03-20 09:13:26 +0100

    Add files via upload

---
+++ PanAffichage.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+
+public class PanAffichage extends JPanel{
+    
+    // Attributs
+    
+    public PanAffichage(int LARGEUR, int HAUTEUR){
+        
+    }
+    
+    public void paintComponent(Graphics g){
+        
+    }
+}
