6009a22044f4bd52b93465988d4a92642006894f
MasterMaevis <gluealafraise@hotmail.fr>
2017-03-27 09:49:51 +0200

    Update PanJeu.java

--- Code/PanJeu.java
+++ Code/PanJeu.java
@@ -1,3 +1,4 @@
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -6,27 +7,42 @@ import java.awt.geom.*;
 import java.awt.image.*;
 import javax.swing.JPanel;
 import java.util.LinkedList ;
-import java.awt.Graphics;
 
 
 public class PanJeu extends PanAffichage implements KeyListener, ActionListener {
-	
+        
     // Attibuts
-	private Partie partie;
+    private Partie partie;
+    private int LARGEUR;
+    private int HAUTEUR;
     
     // Touches clavier
     private boolean toucheGauche;
     private boolean toucheDroit;
     private boolean toucheSpace;
-	
+        
     // * * * TIMER * * *
     private int temps;
     private final int TPS_TIMER_MS = 100;
     private Timer monTimer;
     
-	public PanJeu(int LARGEUR, int HAUTEUR, Timer monTimer, Partie p){
+    // Affichage 
+    private Image imageFond;
+    private BufferedImage fond;
+    
+    public PanJeu(int LARGEUR, int HAUTEUR, Partie p){
         super(LARGEUR, HAUTEUR);
-		partie = p;
+        this.LARGEUR = LARGEUR;
+        this.HAUTEUR = HAUTEUR;
+        partie = p;
+        this.setBackground(Color.GREEN);
+        
+        // Initialisation du buffer
+        fond = new BufferedImage(LARGEUR,HAUTEUR,BufferedImage.TYPE_INT_RGB);
+        
+        // Chargement de l'image d'arrière plan
+        Toolkit t = Toolkit.getDefaultToolkit();
+        imageFond = t.getImage("fond.jpg");  
         
         // Evenement clavier
         toucheGauche = false;
@@ -38,11 +54,28 @@ public class PanJeu extends PanAffichage implements KeyListener, ActionListener 
         monTimer = new Timer(TPS_TIMER_MS,this);
         temps = 0;
         monTimer.start();
-	}
+    }
     
+    public int getTemps(){
+        return temps;
+    }
+        
     public void actionPerformed(ActionEvent e){
         // actualisation du jeu à chaque pas de temps
         temps += TPS_TIMER_MS;
+        if(toucheDroit){
+            partie.getPersonnage().setDirection(0);
+            partie.getPersonnage().bouger(this);
+        }
+        if(toucheGauche){
+            partie.getPersonnage().setDirection(Math.PI);
+            partie.getPersonnage().bouger(this);
+        }
+        if(toucheSpace){
+            Missile nouvMissile = new Missile(partie.getPersonnage().getX(),LARGEUR,HAUTEUR);
+            partie.getListeMissiles().add(nouvMissile);
+        }
+        repaint();
     }
         
     public void keyTyped(KeyEvent e) {}
@@ -65,18 +98,21 @@ public class PanJeu extends PanAffichage implements KeyListener, ActionListener 
             case KeyEvent.VK_RIGHT : toucheDroit=false; break;    
         }
     }
-	
-	public void paintComponent(Graphics g){
-	
-		super.paintComponent(g);
-
-		partie.getPersonnage().dessiner(g, this);
-		
-		for(int i = 0 ; i< partie.getListeMissiles().size() ; i++){
-			partie.getListeMissiles().get(i).dessiner(g, this);
-		}
-		for(int i = 0 ; i< partie.getListeBalles().size() ; i++){
-			partie.getListeBalles().get(i).dessiner(g, this);
-		}
-	}
+        
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            // préparation de l'affichage en dessinant dans le buffer
+            Graphics buffer = fond.getGraphics(); // récupération de l'objet Graphics associé au buffer
+            buffer.drawImage(imageFond,0,0,this); // dessin dans cet objet Graphics
+            // affichage du contenu du buffer
+            g.drawImage(fond,0,0,this);
+            partie.getPersonnage().dessiner(g, this);
+            for(int i = 0 ; i< partie.getListeMissiles().size() ; i++){
+                    partie.getListeMissiles().get(i).dessiner(g, this);
+            }
+            for(int i = 0 ; i< partie.getListeBalles().size() ; i++){
+                    partie.getListeBalles().get(i).bouger(this);
+                    partie.getListeBalles().get(i).dessiner(g, this);
+            }
+        }
 }
