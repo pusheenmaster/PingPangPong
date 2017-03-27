5717e7e0f68924b4498c3ced6b177f1f7dfe009c
quelquune <chloe.cros618@gmail.com>
2017-03-27 08:46:32 +0200

    Update Balle.java

--- Code/Balle.java
+++ Code/Balle.java
@@ -12,17 +12,19 @@ public class Balle extends Objet {
 	
     private int numero;
     private int hauteurMax;
-    private boolean monte; // pas utilisé, pour tests
-    private int sens; // pas utilisé, pour tests
+    private boolean monte;
+    private int sens;
+    private boolean rebond1;
     
     /** Balle qui part de gauche a droite **/
 	public Balle(int numero, JPanel jp){
-        //super(200, 0 ,("balle_"+numero+".png"), 2*Math.PI/3, 2 ); // toutes les balles initialisée pareil
-        super(600, 200 ,"balle.png", 2*Math.PI/3, 30 );
+        //super(600, 200 ,("balle_"+numero+".png"), 2*Math.PI/3, numero*2 ); // toutes les balles initialisée pareil
+        super(600, 100 ,"balle.png", 2*Math.PI/3, 30 );
         this.numero = numero;
-        hauteurMax = 100; // a changer en fonction numero
+        hauteurMax = 100*numero; // a changer en fonction numero
         monte = false;
         sens = 1;
+        rebond1 = false;
     }
     
     public int getNumero(){
@@ -37,11 +39,11 @@ public class Balle extends Objet {
 		return direction;
 	}
 	
-	public boolean getMonte(){  // pour tests
+	public boolean getMonte(){
 		return monte;
 	}
 	
-	public int getSens(){  // pour tests
+	public int getSens(){
 		return sens;
 	}
     
@@ -82,22 +84,29 @@ public class Balle extends Objet {
 		
 		super.bouger();
 		
-		if(x<0){
+		if(!rebond1){
+			super.bouger();
+		}
+		if(!rebond1 && (x<0 ||  x > jp.getWidth()-largeur   ||  y > jp.getHeight() - hauteur)  ){
+			rebond1 = true;
+		}
+		
+		if(rebond1 && x<0){
 			x = 0;
 			changeDirection();
 			
 		}
-		if(x> jp.getWidth()-largeur){
+		if(rebond1 &&  x > jp.getWidth()-largeur){
 			x = jp.getWidth()- largeur;
 			changeDirection();
 		}
-		if(y < hauteurMax){
+		if(rebond1 &&  y < hauteurMax){
 			y = hauteurMax;
 			changeDirection();
 			//changeSens();
 			vitesse = -vitesse;
 		}
-		if(y > jp.getHeight() - hauteur){
+		if(rebond1 && y > jp.getHeight() - hauteur){
 			y = jp.getHeight() - hauteur;
 			changeDirection();
 			//changeSens();
