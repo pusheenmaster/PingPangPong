97ecd42ceb51037a9278c99ea10aee6e3d67ae2c
MasterMaevis <gluealafraise@hotmail.fr>
2017-04-03 09:56:34 +0200

    Update PanFin.java

--- Code/PanFin.java
+++ Code/PanFin.java
@@ -1,17 +1,43 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.StyledDocument;
 
-public class PanFin extends JPanel {
+public class FenetreFin extends JFrame {
 	
 	 
-	public PanFin(){
-		this.setLayout(new BorderLayout());
-		this.setBackground(Color.DARK_GRAY);
+	public FenetreFin(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(800, 500);
+		this.setLocationRelativeTo(null);
+        
+        JPanel panelPrincipal = new JPanel();
+		panelPrincipal.setLayout(new BorderLayout());
+		panelPrincipal.setBackground(Color.PINK);
+        
+        JPanel panelSecondaire = new JPanel();
+		panelSecondaire.setLayout(new GridLayout());
+		panelSecondaire.setBackground(Color.BLUE);
+		
 		JLabel image = new JLabel (new ImageIcon("Fin.png"));
-		this.add(image, BorderLayout.NORTH);
+        Dimension dim = new Dimension(580,180);
+		image.setPreferredSize(dim);
 		
+		JTextArea texte = new JTextArea();
+        texte.setText("hello");
+        
+        JTextArea chiffres = new JTextArea();
+        chiffres.setText("3");
+        
+        panelSecondaire.add(texte);
+		panelSecondaire.add(chiffres);
+        panelPrincipal.add(panelSecondaire, BorderLayout.CENTER);
+		panelPrincipal.add(image, BorderLayout.NORTH);
 		
+		this.setContentPane(panelPrincipal);
+		this.setVisible(true);
     }
 	
+    public static void main( String[] args){
+        FenetreFin f = new FenetreFin();
+    }
 }
-
