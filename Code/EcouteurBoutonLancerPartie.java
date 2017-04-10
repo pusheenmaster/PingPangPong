bd1323c7aa79da0ecd8df1e43b1f4eac8a3ab1df
BabyOtter <MilieDef@gmail.com>
2017-04-10 09:20:58 +0200

    Update EcouteurBoutonLancerPartie.java

--- Code/EcouteurBoutonLancerPartie.java
+++ Code/EcouteurBoutonLancerPartie.java
@@ -7,10 +7,17 @@ public class EcouteurBoutonLancerPartie implements ActionListener{
 	
 	private PanPerso pan;
 	
+       /**Constructeur de EcouteurBoutonLancerPartie
+       *@param pan (PanPerso)
+       **/
 	public EcouteurBoutonLancerPartie(PanPerso pan){
 		this.pan = pan;
 	}
 	
+       /**La méthode ActionPerformed(e) ouvre un nouveau panel de jeu avec le nom choisi, le personnage et le fond
+       *associé et affiche "nouveau jeu lancé" dans le terminal
+       *@param e (ActionEvent)
+       **/
 	public void actionPerformed(ActionEvent e){
 		pan.getJoueur().setNom(pan.getTextField().getText());
 		FenetreJeu fenJeu= new FenetreJeu(pan.getJoueur());
