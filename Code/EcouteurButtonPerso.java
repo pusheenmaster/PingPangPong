478573e5c20dd3cfadc7e7cb74c28388d66f9bec
BabyOtter <MilieDef@gmail.com>
2017-04-10 09:23:54 +0200

    Update EcouteurButtonPerso.java

--- Code/EcouteurButtonPerso.java
+++ Code/EcouteurButtonPerso.java
@@ -7,10 +7,16 @@ import java.util.EventObject;
 public class EcouteurButtonPerso implements ActionListener {
     private PanPerso pan;
 
+    /**Constructeur de EcouteurButtonPerso
+    *@param pan (PanPerso)
+    **/
     public EcouteurButtonPerso(PanPerso pan) {
         this.pan=pan;
     }
 
+    /**La méthode actionPerformed(ae) définit le personnage et le fond qui s'affichera pour la partie
+    *@param ae (ActionEvent)
+    **/	
     public void actionPerformed(ActionEvent ae) {
 		JButton b = (JButton) (ae.getSource());
 		pan.getJoueur().setPerso(new Personnage(b.getIcon().toString(),900,700)); //choisir un personnage
