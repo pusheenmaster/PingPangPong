5512824e2eac208ffc3ebfb4eb39055f0b06fe7b
BabyOtter <MilieDef@gmail.com>
2017-04-10 09:53:01 +0200

    Update Missile.java

--- Code/Missile.java
+++ Code/Missile.java
@@ -12,6 +12,9 @@ public class Missile extends Objet {
     private int taille; //taille rectangle fleche
     private boolean enHaut;
     
+    /**Constructeur de Missile, instancie sa taille, sa postisiont et qu'il n'a pas atteind le haut de la fenêtre
+    *@param xPerso (int), largPan (int), heutPan (int)
+    **/
     public Missile(int xPerso, int largPan, int hautPan ) {   
         super(xPerso, 0, "missile.png", -Math.PI/2, 0, 80);
         y = hautPan - hauteur;
@@ -20,20 +23,33 @@ public class Missile extends Objet {
         
     }
   
+    /**la méthode getTaille() retourne la taille du missile
+    *@return taille (int)
+    **/
     public int getTaille(){
 		return taille;
 	}
 	
-	public boolean getEnHaut(){
+    /**la méthode getEnHaut() dit si le missile a atteind le heut de la fenêtre
+    *@return enHaut (boolean)
+    **/
+    public boolean getEnHaut(){
 		return enHaut;
 	}
 	
+    /**la méthode dessiner(g, fen) affiche le missile
+    *@param g (Graphics), fen (JFrame)
+    **/
     public void dessiner(Graphics g , JFrame fen){
         super.dessiner(g, fen);
         g.setColor(Color.BLACK);
         g.fillRect((int)(x + largeur/2), fen.getHeight() - taille , 5, taille);
     }
 
+    /**la méthode bouger(fen) fait monter le missile
+    *@param fen (JFrame)
+    *@return boolean
+    **/
     public boolean bouger(JFrame fen){
 		if(y<=20){
 			enHaut = true;
