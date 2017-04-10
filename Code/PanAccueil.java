c7391e43b367d4312462fbeb3b0d5bf2164047da
BabyOtter <MilieDef@gmail.com>
2017-04-10 20:47:28 +0200

    Update PanAccueil.java

--- Code/PanAccueil.java
+++ Code/PanAccueil.java
@@ -7,6 +7,8 @@ import java.awt.image.*;
 
 public class PanAccueil extends JPanel{
     
+    /**Constructeur de PanAccueil, affiche l'image d'accueil
+    **/
     public PanAccueil(){
 		JLabel J = new JLabel (new ImageIcon("Accueil.png"));
 		this.add(J);
